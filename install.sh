caf125d1ec9dd5890701ffba70d6b4d3dfe50fd3
Erik von Brömssen <erik.von.b@hotmail.com>
2025-10-24 23:48:13 +0200

    install.sh prints optional commands for convenience

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
+NVIM_DIR="$HOME/.config/nvim"
+NVIM_PACK_DIR="$NVIM_DIR/pack"
+NVIM_COLORSCHEME_DIR="$NVIM_PACK_DIR/default/start"
+
+echo "Stowing mandatory files..."
 stow -v -t ~ stow/
+echo "Done."
+
+echo ""
+
+echo "==== Optionals: ===="
+echo "nvim: gruvbox"
+echo "git clone https://github.com/morhetz/gruvbox $NVIM_COLORSCHEME_DIR/gruvbox/"
+echo "git clone https://github.com/sainnhe/gruvbox-material $NVIM_COLORSCHEME_DIR/gruvbox-material/"
+
+echo "nvim: fugitive"
+echo "git clone https://tpope.io/vim/fugitive.git $NVIM_PACK_DIR/tpope/start/fugitive/"

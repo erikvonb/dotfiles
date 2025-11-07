13151029ed396dba8f3dbdd7a6fba6cb69a1c860
Erik von Brömssen <erik.von.b@hotmail.com>
2025-11-07 21:07:39 +0100

    install.sh: fzf.vim

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/bash
 
+# TRUE=0
+# FALSE=1
 NVIM_DIR="$HOME/.config/nvim"
 NVIM_PACK_DIR="$NVIM_DIR/pack"
 NVIM_COLORSCHEME_DIR="$NVIM_PACK_DIR/default/start"
+
+# ask() {
+#     local name="$1"
+#     read -p "Install ${name}? (y/n) "
+#     case $REPLY in
+#         [Yy]*)
+#             echo "Installing..."
+#             return $TRUE
+#             ;;
+#         [Nn]*)
+#             echo "Skipping"
+#             ;;
+#         *)
+#             echo "Unknown answer, skipping"
+#             ;;
+#     esac
+#     return $FALSE
+# }
 
 echo "Stowing mandatory files..."
 stow -v -t ~ stow/
@@ -20,3 +40,7 @@ echo "git clone https://tpope.io/vim/fugitive.git $NVIM_PACK_DIR/tpope/start/fug
 
 echo "nvim: lsp"
 echo "git clone https://github.com/neovim/nvim-lspconfig $NVIM_PACK_DIR/nvim/start/nvim-lspconfig"
+
+echo "nvim: fzf.vim"
+echo "git clone https://github.com/junegunn/fzf.vim.git $NVIM_PACK_DIR/junegunn/start/fzf.vim"
+
